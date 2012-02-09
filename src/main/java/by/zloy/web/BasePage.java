f5b29605c0783443db3b06d22372584fc0d3d06c
ZloyBY@gmail.com <ZloyBY@gmail.com@ea976c7b-9087-c5a1-6bd6-43b63f03a256>
2012-02-09 08:21:27 +0000

    Working click+gae application, clean code

--- src/main/java/by/zloy/web/BasePage.java
+++ src/main/java/by/zloy/web/BasePage.java
@@ -9,15 +9,6 @@ import org.apache.click.Page;
  */
 public class BasePage extends Page{
 
-//    protected Logger log;
-//
-//    public Logger getLogger() {
-//        if (log == null) {
-//            log = Logger.getLogger(this.getClass());
-//        }
-//        return log;
-//    }
-
     @Override
     public String getTemplate() {
         return "/base.htm";
