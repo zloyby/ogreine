2c98405714a365107b05a2f5115593731e79b6ad
Eugene Kozlov <EugeneKozlov@actovate.com>
2016-12-19 15:03:04 +0100

    Moved to new google cloud and remove outdated datanucleus library

--- src/main/java/by/zloy/web/IndexPage.java
+++ src/main/java/by/zloy/web/IndexPage.java
@@ -9,23 +9,7 @@ public class IndexPage extends BasePage {
 
     public String title = this.getMessage("index.title");
 
-/*
-    private MessageRepository messageRepository = new MessageRepository();
-*/
-
     public IndexPage() {
         addModel("time", new Date());
     }
-
-/*
-    protected void createMessage() {
-        Message message = new Message();
-        message.setText(new Date().toString());
-        messageRepository.create(message);
-    }
-
-    protected void deleteMessage(Long id) {
-        messageRepository.deleteById(id);
-    }
-*/
 }
